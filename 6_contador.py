07bc853efd1ed9eba54b6dc1a298a8059c0ef99b
danidani1907 <danielli.freitas1907@gmail.com>
2025-09-01 16:34:17 -0300

    Update 6_contador.py

--- 6_contador.py
+++ 6_contador.py
@@ -15,6 +15,20 @@ def main(page: ft.Page):
         text_align=ft.TextAlign.CENTER
     )
 
-    
+    info_contador = ft.Text(
+        value="Contador iniciado em 0",
+        size=20,
+        color=ft.Colors.PURPLE,
+        text_align=ft.TextAlign.CENTER
+    )
+
+    def atualizar_display():
+        # Atualiza o texto do display com o valor atual do contador
+        display_contador.value = str(valor_contador)
+
+        if valor_contador > 0:
+           display_contador.value = "Contador positivo! 🎉"
+        info_contador.color = ft.Colors.GREEN
+
 
   
